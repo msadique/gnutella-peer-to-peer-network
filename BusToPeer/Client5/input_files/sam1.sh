e47f542b7dbd6c4320711d45b941ec19f57d8d0f
Mohd Sadique <msadique@hawk.iit.edu>
2017-03-19 17:42:55 -0500

    Add files via upload

---
+++ BusToPeer/Client5/input_files/sam1.sh
@@ -0,0 +1,65 @@
+file="F_1KB.txt"
+for num in $(seq 1 3)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_2KB.txt"
+for num in $(seq 1 6)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_3KB.txt"
+for num in $(seq 1 9)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_4KB.txt"
+for num in $(seq 1 12)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_5KB.txt"
+for num in $(seq 1 15)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_6KB.txt"
+for num in $(seq 1 18)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_7KB.txt"
+for num in $(seq 1 21)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_8kb.txt"
+for num in $(seq 1 24)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_9KB.txt"
+for num in $(seq 1 27)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_10KB.txt"
+for num in $(seq 1 30)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
+
+file="F_100kb.txt"
+for num in $(seq 1 300)
+do
+    echo "A cloud is a computing capability that provides an abstraction between the computing resource and its underlying technical architecture enabling convenient demand network access to a shared pool of configurable  computing resources that can be rapidly provisioned and released with minimal management effort or service provide interaction NIST" >> $file
+done
---
+++ BusToPeer/Client5/peerClientThread.java
@@ -0,0 +1,196 @@
+
+/*******************************************************************************
+ *  FILE NAME    : peerClientThread.java
+ *
+ *  DESCRIPTION  : This class take the client requestType and calls related functions 
+ *
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ *          HEADER FILES
+ ******************************************************************************/
+import java.io.*;
+import java.net.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class peerClientThread extends Thread
+{
+	Socket socket;
+	Path currentRelativePath = Paths.get("");  // Get current path
+	String cpath = currentRelativePath.toAbsolutePath().toString();
+	
+	public peerClientThread()
+	{
+		
+	}
+	//This function accept the client request and call the respective functions
+	public void run() 
+	{
+		Scanner scanner=new Scanner(System.in);
+		while(true)
+		{
+			System.out.println("\n\n\t1. Lookup Files");
+			System.out.println("\t2. Exit ");
+			System.out.print("\tEnter :- ");
+			int input = scanner.nextInt();
+			if( input == 1 )
+			{
+				
+	        		scanner=new Scanner(System.in);
+				System.out.print("\n\n Enter File name :- ");
+        			 String input1 = scanner.next();	
+				for(int j=0;j<Client.Repeat;j++){
+				RequestPeerList(input1);  // This function access all the peer names and information associated with it from the server.
+				try {
+    					Thread.sleep(Client.WaitTime*1000);                 //1000 milliseconds is one second.
+				} catch(InterruptedException ex) {
+    					Thread.currentThread().interrupt();
+				}	
+			//}
+				if(Client.peersResponse.size()!=0){	
+				
+				Client.avgTime = Client.tTotalTime/Client.peersResponse.size();
+	 			System.out.println( "---------------------------------------------------------------------------------" );
+        			System.out.println( "Average Response Time from '"+Client.peersResponse.size()+ "' Clients in Milliseconds: " +(Client.avgTime/1000000) );
+	 			System.out.println( "---------------------------------------------------------------------------------" );
+			
+			//if( input == 2 ){
+				System.out.println("************    Download file    ***********");
+				for(int i=0;i<Client.peersResponse.size();i++){
+					System.out.println(" "+(i+1)+".   Client"+(i+1)+"   IP:"+Client.peersResponse.get(i).getIpAddr()+"  Port:"+Client.peersResponse.get(i).getPort()+ "   Response Time :- "+(Client.peersResponse.get(i).getTime()/1000000)+"ms");
+				}
+	        		scanner=new Scanner(System.in);
+                		System.out.print("\n\nSelect Client to Download (Please Enter No.) :-  ");
+                		input1 = scanner.next();
+				int pos = Integer.parseInt(input1);
+				DownloadFile(pos-1);  // This function access all the peer names and information associated with it from the server.
+			}else
+			System.out.print("File Not Found");
+			}}
+			else if( input == 2 )
+			{	
+				System.exit(0);
+			}
+		}
+	}
+	private void RequestPeerList(String input)
+	{
+		
+		System.out.println("************    Lookup for file    ***********");
+		Client.sTime = System.nanoTime();
+		Client.avgTime=0;
+		 Client.tTotalTime =0;
+		Client.peersResponse.clear();
+		Query query = new Query();
+		Client.messageID = ThreadLocalRandom.current().nextInt(Client.ClientPort, (Integer.MAX_VALUE/2)); 
+		query.setMessageID(Client.messageID);
+		query.setTtl(Client.TTL);
+		query.setIpAddr(Client.ClientIP);
+		query.setPort(Client.ClientPort);
+		query.setFileName(input);
+		query.setQueryType(QueryType.Request);
+		Client.queryList.add(query);
+
+		sendObject2AllPeer(query);
+	}
+	private void DownloadFile(int inP)
+	{
+		int pos = inP;
+		try{
+			long lStartTime = System.nanoTime();
+			receiveFile(Client.peersResponse.get(pos));
+			long lEndTime = System.nanoTime();
+ long output = lEndTime - lStartTime;
+        File file =new File( cpath + "/output_files/" + Client.peersResponse.get(pos).getFileName( ) );
+        if( file.exists( ) )
+        {
+                double bytes = file.length( );
+                double kilobytes = ( bytes / 1024 );
+                double megabytes = ( kilobytes / 1024 );
+                DecimalFormat df = new DecimalFormat( );
+                df.setMaximumFractionDigits( 2 );
+                System.out.println( "---------------------------------------------------------------------------------" );
+        System.out.println( "Transfer Time in milliseconds: " + output / 1000000 );
+
+        if( megabytes < 1 )
+         System.out.println( "\nDownloaded file :" + Client.peersResponse.get(pos).getFileName() + " File size : " + df.format( kilobytes)+"KB");
+        else
+        System.out.println("\nDownloaded file :"+Client.peersResponse.get(pos).getFileName()+" File size : "+df.format(megabytes)+"MB");
+        System.out.println("\nPerformance speed "+ df.format((megabytes*1000000*1000)/output)+"MB/sec");
+                System.out.println("---------------------------------------------------------------------------------");
+        }
+	 	}
+		catch(Exception e)
+                {
+                        System.out.println(e.getMessage());
+                }
+	
+
+	}	
+	private void sendObject2AllPeer(Query query)
+	{
+		for(int i=0;i<Client.peers.size();i++){
+		try 
+		{
+                        {
+                             	socket=new Socket(Client.peers.get(i).getIpAddr(),Client.peers.get(i). getPort());
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				oos.writeObject(query);
+				socket.close();
+			}
+		} 
+		catch(Exception e)
+		{
+			//System.out.println(e.getMessage());
+		}
+		}
+		
+	}
+
+	private void sendObject2Peer(Query query){
+		try 
+		{
+			socket=new Socket(query.getIpAddr(),query.getPort());
+			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+			oos.writeObject(query);
+		} 
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}
+		
+	
+	//@SuppressWarnings("resource")
+	
+	//This function receive the file and its content from the client which has the reuested file.	
+	private int receiveFile(Query query) throws UnknownHostException, IOException
+	{
+			
+			query.setQueryType(QueryType.Download);
+			//peerConversation=true;
+			System.out.println("sending request to peer");
+			sendObject2Peer(query);
+			FileOutputStream OS=null; 
+			InputStream IS=null;
+			// have to either get file or file not found
+			IS=socket.getInputStream();
+			OS= new FileOutputStream(cpath+"/output_files/"+query.getFileName());
+			int bytesRead=0;
+			byte[] b=new byte[512000]; 
+			while((bytesRead=IS.read(b, 0, b.length))!=-1)
+			{ 
+				OS.write(b, 0, bytesRead);	
+			}
+			OS.close();
+			return 0;
+	}		
+}
